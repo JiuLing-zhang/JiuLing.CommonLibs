5110b78d4f60892fdeb98049d5fc2a0e0b26dacb
九零 <JiuLing-zhang@outlook.com>
2021-07-16 13:46:55 +0800

    新增部分正则方法

--- src/Text/RegexHelper.cs
+++ src/Text/RegexHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace JiuLing.CommonLibs.Text
 {
@@ -8,7 +9,7 @@ namespace JiuLing.CommonLibs.Text
     public class RegexHelper
     {
         /// <summary>
-        /// 指示所指定的正则表达式在指定的输入字符串中是否找到了匹配项。。
+        /// 指示所指定的正则表达式在指定的输入字符串中是否找到了匹配项。
         /// </summary>
         /// <param name="input">要搜索匹配项的字符串。</param>
         /// <param name="pattern">要匹配的正则表达式模式。</param>
@@ -16,6 +17,56 @@ namespace JiuLing.CommonLibs.Text
         public bool IsMatch(string input, string pattern)
         {
             return Regex.IsMatch(input, pattern);
+        }
+
+        /// <summary>
+        /// 根据正则表达式获取输入字符串中的第一个匹配项
+        /// </summary>
+        /// <param name="input">要搜索匹配项的字符串。</param>
+        /// <param name="pattern">要匹配的正则表达式模式。</param>
+        /// <returns>如果没有匹配到任何项，返回null,否则返回第一个</returns>
+        public string GetFirst(string input, string pattern)
+        {
+            MatchCollection mc = Regex.Matches(input, pattern);
+            if (mc.Count == 0)
+            {
+                return null;
+            }
+            return mc[0].Value;
+        }
+
+        /// <summary>
+        /// 根据正则表达式获取输入字符串中的第一个匹配项
+        /// </summary>
+        /// <param name="input">要搜索匹配项的字符串。</param>
+        /// <param name="pattern">要匹配的正则表达式模式。</param>
+        /// <returns>如果没有匹配到任何项，返回string.Empty,否则返回第一个</returns>
+        public string GetFirstOrDefault(string input, string pattern)
+        {
+            string result = GetFirst(input, pattern);
+            return result ?? string.Empty;
+        }
+
+        /// <summary>
+        /// 根据正则表达式获取输入字符串中的所有匹配项
+        /// </summary>
+        /// <param name="input">要搜索匹配项的字符串。</param>
+        /// <param name="pattern">要匹配的正则表达式模式。</param>
+        /// <returns>如果没有匹配到任何项，返回null,否则返回匹配到的所有项</returns>
+        public List<string> GetAll(string input, string pattern)
+        {
+            MatchCollection mc = Regex.Matches(input, pattern);
+            if (mc.Count == 0)
+            {
+                return null;
+            }
+
+            var result = new List<string>();
+            foreach (Match item in mc)
+            {
+                result.Add(item.Value);
+            }
+            return result;
         }
     }
 }
--- test/Text/RegexHelperTests.cs
+++ test/Text/RegexHelperTests.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using JiuLing.CommonLibs.Text;
+using System.Linq;
 
 namespace JiuLing.CommonLibs.TextTests
 {
@@ -14,6 +16,53 @@ namespace JiuLing.CommonLibs.TextTests
         public void IsMatchTest(string input, string pattern, bool result)
         {
             Assert.AreEqual(_regexHelper.IsMatch(input, pattern), result);
+        }
+
+        [TestMethod()]
+        [DataRow("test1", @"test\d{2}", null)]
+        [DataRow("test12", @"test\d{2}", "test12")]
+        [DataRow("test123", @"test\d{2}", "test12")]
+        [DataRow("test123test456", @"test\d{2}", "test12")]
+        public void GetFirstTest(string input, string pattern, object result)
+        {
+            Assert.AreEqual(_regexHelper.GetFirst(input, pattern), result);
+        }
+
+        [TestMethod()]
+        [DataRow("test1", @"test\d{2}", "")]
+        [DataRow("test12", @"test\d{2}", "test12")]
+        [DataRow("test123", @"test\d{2}", "test12")]
+        [DataRow("test123test456", @"test\d{2}", "test12")]
+        public void GetFirstOrDefaultTest(string input, string pattern, string result)
+        {
+            Assert.AreEqual(_regexHelper.GetFirstOrDefault(input, pattern), result);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            string input;
+            string pattern;
+            List<string> result;
+            List<string> realResult;
+
+            input = "test1";
+            pattern = @"test\d{2}";
+            result = null;
+            realResult = _regexHelper.GetAll(input, pattern);
+            Assert.IsTrue(realResult == result);
+
+            input = "test123";
+            pattern = @"test\d{2}";
+            result = new List<string>() { "test12" };
+            realResult = _regexHelper.GetAll(input, pattern);
+            Assert.IsTrue(realResult.SequenceEqual(result));
+
+            input = "test123test456";
+            pattern = @"test\d{2}";
+            result = new List<string>() { "test12", "test45" };
+            realResult = _regexHelper.GetAll(input, pattern);
+            Assert.IsTrue(realResult.SequenceEqual(result));
         }
     }
 }
