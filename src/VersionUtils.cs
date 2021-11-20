70a9000e822846adeb897506e1db4e661f6e78c2
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-20 17:48:53 +0800

    新增
    1、新增程序版本帮助类

---
+++ src/VersionUtils.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace JiuLing.CommonLibs
+{
+    /// <summary>
+    /// 程序版本帮助类
+    /// </summary>
+    public class VersionUtils
+    {
+        /// <summary>
+        /// 检查当前版本是否需要更新
+        /// </summary>
+        /// <param name="currentVersion">当前版本</param>
+        /// <param name="newVersion">最新版本</param>
+        /// <returns>返回是否需要更新</returns>
+        public static bool CheckNeedUpdate(Version currentVersion, Version newVersion)
+        {
+            return currentVersion.CompareTo(newVersion) < 0;
+        }
+        /// <summary>
+        /// 检查当前版本是否需要更新
+        /// </summary>
+        /// <param name="currentVersion">当前版本</param>
+        /// <param name="newVersion">最新版本</param>
+        /// <param name="minVersion">最小运行版本</param>
+        /// <returns>返回（是否需要自动更新，当前版本是否允许使用）</returns>
+        public static (bool IsNeedUpdate, bool IsAllowUse) CheckNeedUpdate(Version currentVersion, Version newVersion, Version minVersion)
+        {
+            if (!CheckNeedUpdate(currentVersion, newVersion))
+            {
+                return (false, true);
+            }
+
+            if (currentVersion.CompareTo(minVersion) < 0)
+            {
+                return (true, false);
+            }
+
+            return (true, true);
+        }
+    }
+}
---
+++ test/VersionUtilsTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JiuLing.CommonLibs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuLing.CommonLibs.Tests
+{
+    [TestClass()]
+    public class VersionUtilsTests
+    {
+        [TestMethod()]
+        [DataRow("1.0.0", "1.0.0", false)]
+        [DataRow("1.0.0", "1.0.2", true)]
+        [DataRow("2.0.0.0", "2.0.0.0", false)]
+        [DataRow("2.1.0.0", "3.0.0.0", true)]
+        public void CheckNeedUpdateTest(string currentVersion, string newVersion, bool isNeedUpdate)
+        {
+            Assert.IsTrue(isNeedUpdate == VersionUtils.CheckNeedUpdate(new Version(currentVersion), new Version(newVersion)));
+        }
+
+        [TestMethod()]
+        [DataRow("1.0.0", "1.0.2", "1.0.0", true, true)]
+        [DataRow("1.0.0", "1.0.2", "1.0.1", true, false)]
+        [DataRow("1.0.0", "1.0.0", "1.0.0", false, true)]
+        public void CheckNeedUpdate2Test(string currentVersion, string newVersion, string minVersion, bool isNeedUpdate, bool isAllowUse)
+        {
+            var cv = new Version(currentVersion);
+            var nv = new Version(newVersion);
+            var mv = new Version(minVersion);
+            var result = VersionUtils.CheckNeedUpdate(cv, nv, mv);
+            Assert.IsTrue(result == (isNeedUpdate, isAllowUse));
+        }
+    }
+}
