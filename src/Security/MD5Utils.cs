dd05aa85a819e45ba6d78b9208853b4299ab59ee
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-21 19:42:23 +0800

    新增
    1、字符串的MD5值计算

---
+++ src/Security/MD5Utils.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace JiuLing.CommonLibs.Security
+{
+    /// <summary>
+    /// MD5的帮助类
+    /// </summary>
+    public class MD5Utils
+    {
+        /// <summary>
+        /// 编码格式
+        /// </summary>
+        public static Encoding MyEncoding = Encoding.UTF8;
+
+        /// <summary>
+        /// 计算字符串的32位MD5值（小写）
+        /// </summary>
+        /// <param name="input">要计算的字符串</param>
+        /// <returns>返回字符串的MD5值</returns>
+        public static string GetLowerValue(string input)
+        {
+            return GetUpperValue(input).ToLower();
+        }
+
+        /// <summary>
+        /// 计算字符串的32位MD5值（大写）
+        /// </summary>
+        /// <param name="input">要计算的字符串</param>
+        /// <returns>返回字符串的MD5值</returns>
+        public static string GetUpperValue(string input)
+        {
+            var md5 = new MD5CryptoServiceProvider();
+            var byteValue = MyEncoding.GetBytes(input);
+            var byteHash = md5.ComputeHash(byteValue);
+            md5.Clear();
+            string tmpValue = "";
+            for (int i = 0; i < byteHash.Length; i++)
+            {
+                tmpValue += byteHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return tmpValue;
+        }
+    }
+}
---
+++ test/Security/MD5UtilsTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JiuLing.CommonLibs.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiuLing.CommonLibs.Security.Tests
+{
+    [TestClass()]
+    public class MD5UtilsTests
+    {
+        [TestMethod()]
+        [DataRow("test", "098f6bcd4621d373cade4e832627b4f6")]
+        public void GetLowerValueTest(string input, string md5)
+        {
+            Assert.IsTrue(MD5Utils.GetLowerValue(input) == md5);
+        }
+
+        [TestMethod()]
+        [DataRow("test", "098F6BCD4621D373CADE4E832627B4F6")]
+        public void GetUpperValueTest(string input, string md5)
+        {
+            Assert.IsTrue(MD5Utils.GetUpperValue(input) == md5);
+        }
+    }
+}
