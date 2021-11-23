81252fd8bd34047534bc65969cbd0cd01c666162
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2021-11-23 17:37:20 +0800

    删除
    1、删除sha1的单元测试用例

--- test/Security/SHA1UtilsTests.cs
+++ test/Security/SHA1UtilsTests.cs
@@ -9,22 +9,19 @@ using System.Threading.Tasks;
 namespace JiuLing.CommonLibs.Security.Tests
 {
     [TestClass()]
+    [Ignore("测试需要使用外部文件，因此忽略")]
     public class SHA1UtilsTests
     {
         [TestMethod()]
-        [DataRow("Security\\test.txt", "f2d974f36a4ea1be3d157b2129a9e0080c680c29")]
-        public void GetFileLowerValueTest(string fileName, string sha1)
+        public void GetFileLowerValueTest()
         {
-            var path = $"{AppDomain.CurrentDomain.BaseDirectory}{fileName}";
-            Assert.IsTrue(SHA1Utils.GetFileLowerValue(path) == sha1);
+            Assert.Fail();
         }
 
         [TestMethod()]
-        [DataRow("Security\\test.txt", "F2D974F36A4EA1BE3D157B2129A9E0080C680C29")]
-        public void GetFileUpperValueTest(string fileName, string sha1)
+        public void GetFileUpperValueTest()
         {
-            var path = $"{AppDomain.CurrentDomain.BaseDirectory}{fileName}";
-            Assert.IsTrue(SHA1Utils.GetFileUpperValue(path) == sha1);
+            Assert.Fail();
         }
     }
 }
