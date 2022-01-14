acca88af482a9442977929067cb5c0487f28d0ae
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-01-14 17:21:14 +0800

    1.Add a model for app upgrade.

---
+++ src/Model/AppUpgradeInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JiuLing.CommonLibs.Model
+{
+    /// <summary>
+    /// 一个通用的App自动更新所需的模型
+    /// </summary>
+    public class AppUpgradeInfo
+    {
+        /// <summary>
+        /// 当前版本
+        /// </summary>
+        public string Version { get; set; }
+        /// <summary>
+        /// 程序运行的最小版本
+        /// </summary>
+        public string MinVersion { get; set; }
+        /// <summary>
+        /// 下载地址
+        /// </summary>
+        public string DownloadUrl { get; set; }
+        /// <summary>
+        /// 更新日志
+        /// </summary>
+        public string Log { get; set; }
+        /// <summary>
+        /// 时间
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+    }
+}
