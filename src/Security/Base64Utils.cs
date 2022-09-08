d80f69db04d46baeb5e1b5955e86ac15a42b45ce
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-09-08 20:14:35 +0800

    1、新增签名计算模块

---
+++ src/Security/Base64Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace JiuLing.CommonLibs.Security
+{
+    /// <summary>
+    /// Base64的工具类
+    /// </summary>
+    public class Base64Utils
+    {
+        /// <summary>
+        /// 编码格式
+        /// </summary>
+        public static Encoding DefaultEncoding = Encoding.UTF8;
+
+        /// <summary>
+        /// 获取字符串的Base64
+        /// </summary>
+        /// <param name="input">输入字符串</param>
+        /// <returns>返回字符串的Base64值</returns>
+        public static string GetStringValue(string input)
+        {
+            byte[] bytes = DefaultEncoding.GetBytes(input);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
