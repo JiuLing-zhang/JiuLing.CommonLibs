88308b9288c76d9e0f782ae82691bab42f135145
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-11-15 16:50:44 +0800

    新增自旋锁

---
+++ src/Threading/SpinLock.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+
+namespace JiuLing.CommonLibs.Threading
+{
+    /// <summary>
+    /// 自旋锁
+    /// </summary>
+    public class SpinLock
+    {
+        private int _lockValue;
+
+        /// <summary>
+        /// 进入
+        /// </summary>
+        public void Enter()
+        {
+            while (true)
+            {
+                if (Interlocked.Exchange(ref _lockValue, 1) == 0)
+                {
+                    return;
+                }
+                //如果有线程正在调用，那么后来的线程就一直在这里“自旋”
+            }
+        }
+
+        /// <summary>
+        /// 离开
+        /// </summary>
+        public void Leave()
+        {
+            Interlocked.Exchange(ref _lockValue, 0);
+        }
+    }
+}
---
+++ test/Threading/SpinLockTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JiuLing.CommonLibs.Threading.Tests
+{
+    [TestClass()]
+    public class SpinLockTests
+    {
+        [TestMethod()]
+        public void EnterTest()
+        {
+            var spinLock = new SpinLock();
+            spinLock.Enter();
+
+            var task = Task.Run(() =>
+            {
+                spinLock.Enter();
+            });
+            Assert.IsFalse(task.Wait(5000));
+        }
+
+        [TestMethod()]
+        public void LeaveTest()
+        {
+            var spinLock = new SpinLock();
+
+            int _count = 0;
+            var tasks = new List<Task>();
+            for (int i = 0; i < 2000; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    spinLock.Enter();
+                    _count = _count + 1;
+                    spinLock.Leave();
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Assert.AreEqual(tasks.Count, _count);
+        }
+    }
+}
