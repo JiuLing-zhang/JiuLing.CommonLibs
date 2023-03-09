b25a37d4cb3ad000ac3471ce11b46f410982c230
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-03-09 19:15:50 +0800

    新增 SHA256Utils

---
+++ src/Security/SHA256Utils.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JiuLing.CommonLibs.Security
+{
+    /// <summary>
+    /// SHA256 的帮助类
+    /// </summary>
+    public class SHA256Utils
+    {
+        /// <summary>
+        /// 编码格式
+        /// </summary>
+        public static Encoding DefaultEncoding = Encoding.UTF8;
+
+        /// <summary>
+        /// 计算文件的哈希值（小写）
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>返回文件的哈希值</returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static string GetFileValueToLower(string fileName)
+        {
+            return GetFileValueToUpper(fileName).ToLower();
+        }
+
+        /// <summary>
+        /// 计算文件的哈希值（大写）
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// <returns>返回文件的哈希值</returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static string GetFileValueToUpper(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                throw new FileNotFoundException(fileName);
+            }
+
+            using (var file = new FileStream(fileName, FileMode.Open))
+            {
+                return GetFileValueToUpper(file);
+            }
+        }
+
+        /// <summary>
+        /// 计算文件的哈希值（小写）
+        /// </summary>
+        /// <param name="stream">文件流</param>
+        /// <returns>返回文件的哈希值</returns>
+        public static string GetFileValueToLower(Stream stream)
+        {
+            return GetFileValueToUpper(stream).ToLower();
+        }
+
+        /// <summary>
+        /// 计算文件的哈希值（大写）
+        /// </summary>
+        /// <param name="stream">文件流</param>
+        /// <returns>返回文件的哈希值</returns>
+        public static string GetFileValueToUpper(Stream stream)
+        {
+            SHA256 sha256 = SHA256.Create();
+            byte[] hash = sha256.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+        /// <summary>
+        /// 计算字符串的哈希值（小写）
+        /// </summary>
+        /// <param name="input">输入字符串</param>
+        /// <returns>返回字符串的哈希值</returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static string GetStringValueToLower(string input)
+        {
+            return GetStringValueToUpper(input).ToLower();
+        }
+
+        /// <summary>
+        /// 计算字符串的哈希值（大写）
+        /// </summary>
+        /// <param name="input">输入字符串</param>
+        /// <returns>返回字符串的哈希值</returns>
+        /// <exception cref="FileNotFoundException"></exception>
+        public static string GetStringValueToUpper(string input)
+        {
+            byte[] inputBytes = DefaultEncoding.GetBytes(input);
+            SHA256 sha256 = SHA256.Create();
+            byte[] hashBytes = sha256.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+    }
+}
---
+++ test/Security/SHA256UtilsTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace JiuLing.CommonLibs.Security.Tests
+{
+    [TestClass()]
+    public class SHA256UtilsTests
+    {
+        [TestMethod()]
+        public void GetFileValueToLowerTest()
+        {
+            string hash = "f29bc64a9d3732b4b9035125fdb3285f5b6455778edca72414671e0ca3b2e0de";
+            string fileName = Path.Combine(Environment.CurrentDirectory, "TestFiles", "FileA.txt");
+            Assert.IsTrue(SHA256Utils.GetFileValueToLower(fileName) == hash);
+        }
+
+        [TestMethod()]
+        public void GetFileValueToUpperTest()
+        {
+            string hash = "F29BC64A9D3732B4B9035125FDB3285F5B6455778EDCA72414671E0CA3B2E0DE";
+            string fileName = Path.Combine(Environment.CurrentDirectory, "TestFiles", "FileA.txt");
+            Assert.IsTrue(SHA256Utils.GetFileValueToUpper(fileName) == hash);
+        }
+
+        [TestMethod()]
+        public void GetFileValueToLowerTest1()
+        {
+            string hash = "f29bc64a9d3732b4b9035125fdb3285f5b6455778edca72414671e0ca3b2e0de";
+            string fileName = Path.Combine(Environment.CurrentDirectory, "TestFiles", "FileA.txt");
+            using var stream = File.OpenRead(fileName);
+            Assert.IsTrue(SHA256Utils.GetFileValueToLower(stream) == hash);
+        }
+
+        [TestMethod()]
+        public void GetFileValueToUpperTest1()
+        {
+            string hash = "F29BC64A9D3732B4B9035125FDB3285F5B6455778EDCA72414671E0CA3B2E0DE";
+            string fileName = Path.Combine(Environment.CurrentDirectory, "TestFiles", "FileA.txt");
+            using var stream = File.OpenRead(fileName);
+            Assert.IsTrue(SHA256Utils.GetFileValueToUpper(stream) == hash);
+        }
+
+        [TestMethod()]
+        [DataRow("hello world", "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9")]
+        [DataRow("Test1", "8a863b145dc6e4ed7ac41c08f7536c476ebac7509e028ed2b49f8bd5a3562b9f")]
+        public void GetStringValueToLowerTest(string input, string result)
+        {
+            Assert.IsTrue(SHA256Utils.GetStringValueToLower(input) == result);
+        }
+
+        [TestMethod()]
+        [DataRow("hello world", "B94D27B9934D3E08A52E52D7DA7DABFAC484EFE37A5380EE9088F7ACE2EFCDE9")]
+        [DataRow("Test1", "8A863B145DC6E4ED7AC41C08F7536C476EBAC7509E028ED2B49F8BD5A3562B9F")]
+        public void GetStringValueToUpperTest(string input, string result)
+        {
+            Assert.IsTrue(SHA256Utils.GetStringValueToUpper(input) == result);
+        }
+    }
+}
