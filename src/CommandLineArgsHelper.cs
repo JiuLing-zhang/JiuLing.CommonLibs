4d4b281f8d690262718a2a1b2caeac5fdf3395e8
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-08-31 20:52:10 +0800

    新增命令行参数解析工具

---
+++ src/CommandLineArgsHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace JiuLing.CommonLibs
+{
+    /// <summary>
+    /// 命令行参数解析工具
+    /// </summary>
+    public class CommandLineArgsHelper
+    {
+        private readonly ConcurrentDictionary<string, List<string>> _args = new ConcurrentDictionary<string, List<string>>();
+
+        /// <summary>
+        /// 使用 Environment.GetCommandLineArgs() 初始化参数
+        /// </summary>
+        public CommandLineArgsHelper()
+        {
+            BuildArgs(string.Join(" ", Environment.GetCommandLineArgs()));
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="args">参数集合</param>
+        public CommandLineArgsHelper(string[] args)
+        {
+            BuildArgs(string.Join(" ", args));
+        }
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="args">参数</param>
+        public CommandLineArgsHelper(string args)
+        {
+            BuildArgs(args);
+        }
+
+        private void BuildArgs(string args)
+        {
+            string pattern = @"(?<=(\s|^))-.*?(?=(?<=(\s|^))-|$)";
+            MatchCollection mc = Regex.Matches(args, pattern);
+            if (mc.Count == 0)
+            {
+                return;
+            }
+
+            foreach (Match m in mc)
+            {
+                string argPart = m.Value.Trim();
+                var argList = new List<string>(argPart.Split(' '));
+
+                string commandKey = argList[0].Trim();
+                argList.RemoveAt(0);
+
+                if (!_args.TryAdd(commandKey, argList))
+                {
+                    throw new ArgumentException($"参数初始化失败：{commandKey}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// 是否包含指定参数
+        /// </summary>
+        /// <param name="key">参数名</param>
+        /// <returns></returns>
+        public bool HasCommand(string key)
+        {
+            return _args.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// 获取指定的参数值
+        /// </summary>
+        /// <param name="key">参数名</param>
+        /// <returns></returns>
+        public List<string> GetCommandValue(string key)
+        {
+            List<string> value;
+            _args.TryGetValue(key, out value);
+            return value;
+        }
+
+        /// <summary>
+        /// 尝试获取指定的参数值
+        /// </summary>
+        /// <param name="key">参数名</param>
+        /// <param name="value">参数值</param>
+        /// <returns></returns>
+        public bool TryGetCommandValue(string key, out List<string> value)
+        {
+            var result = _args.TryGetValue(key, out value);
+            return result;
+        }
+    }
+}
---
+++ test/CommandLineArgsHelperTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JiuLing.CommonLibs.Tests
+{
+    [TestClass()]
+    public class CommandLineArgsHelperTests
+    {
+        private string _argsString = "-type net -value ip port";
+        private string[] _argsArray => _argsString.Split(' ');
+
+        private static IEnumerable<object[]> CommandLineArgs
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[]
+                    {
+                        "-type",
+                        true,
+                        new List<string>(){ "net" }
+                    },
+                    new object[]
+                    {
+                        "-test",
+                        false,
+                        null
+                    },
+                    new object[]
+                    {
+                        "-value",
+                        true,
+                        new List<string>{ "ip", "port" }
+                    }
+                };
+            }
+        }
+
+        [TestMethod()]
+        [DataRow("-type", true)]
+        [DataRow("-test", false)]
+        public void HasCommandTest(string key, bool isExist)
+        {
+            var stringCommand = new CommandLineArgsHelper(_argsString);
+            Assert.IsTrue(stringCommand.HasCommand(key) == isExist);
+            var arrayCommand = new CommandLineArgsHelper(_argsArray);
+            Assert.IsTrue(arrayCommand.HasCommand(key) == isExist);
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(CommandLineArgs))]
+        public void GetCommandValueTest(string key, bool isExist, List<string> value)
+        {
+            var stringCommand = new CommandLineArgsHelper(_argsString);
+            var result = stringCommand.GetCommandValue(key);
+            if (value == null)
+            {
+                Assert.IsTrue(result == value);
+            }
+            else
+            {
+                Assert.IsTrue(result.SequenceEqual(value));
+            }
+
+            var arrayCommand = new CommandLineArgsHelper(_argsArray);
+            result = arrayCommand.GetCommandValue(key);
+            if (value == null)
+            {
+                Assert.IsTrue(result == value);
+            }
+            else
+            {
+                Assert.IsTrue(result.SequenceEqual(value));
+            }
+        }
+
+        [TestMethod()]
+        [DynamicData(nameof(CommandLineArgs))]
+        public void TryGetCommandValueTest(string key, bool isExist, List<string> value)
+        {
+            var stringCommand = new CommandLineArgsHelper(_argsString);
+            List<string> result;
+            var isOk = stringCommand.TryGetCommandValue(key, out result);
+            Assert.IsTrue(isOk == isExist);
+            if (isOk)
+            {
+                Assert.IsTrue(result.SequenceEqual(value));
+            }
+
+            var arrayCommand = new CommandLineArgsHelper(_argsArray);
+            isOk = arrayCommand.TryGetCommandValue(key, out result);
+            Assert.IsTrue(isOk == isExist);
+            if (isOk)
+            {
+                Assert.IsTrue(result.SequenceEqual(value));
+            }
+        }
+    }
+}
