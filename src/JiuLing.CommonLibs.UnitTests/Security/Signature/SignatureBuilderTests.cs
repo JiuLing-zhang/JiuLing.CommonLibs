c12ae1098c58671c26b4160a1edb4252222754c0
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2023-09-09 20:23:37 +0800

    新增签名构建工具

---
+++ src/JiuLing.CommonLibs.UnitTests/Security/Signature/SignatureBuilderTests.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JiuLing.CommonLibs.Security.Signature;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JiuLing.CommonLibs.UnitTests.Security.Signature
+{
+    [TestClass()]
+    public class SignatureBuilderTests
+    {
+        //param1=1&param3=3+&param2=2
+        private readonly Dictionary<string, string> _signSource = new Dictionary<string, string>()
+        {
+            { "param1", "1" },
+            { "param3", "3+" },
+            { "param2", "2" }
+        };
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            var items = _signSource.Select(x => $"{x.Key}={x.Value}").ToList();
+            var result = string.Join("&", items);
+
+            var builder = SignatureBuilder.Create(_signSource);
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3+&param2=2", "param1=1&param3=3+&param2=2")]
+        public void CreateTest2(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input);
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3&param2=2", "1&3&2")]
+        public void FetchParameterValueTest1(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).FetchParameterValue();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3&param2=2", "", "132")]
+        [DataRow("param1=1&param3=3&param2=2", "&", "1&3&2")]
+        public void FetchParameterValueTest2(string input, string separator, string result)
+        {
+            var builder = SignatureBuilder.Create(input).FetchParameterValue(separator);
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3&param2=2", "param11param33param22")]
+        public void FetchParameterNameAndValueTest1(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).FetchParameterNameAndValue();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3&param2=2", "", "param11param33param22")]
+        [DataRow("param1=1&param3=3&param2=2", "+", "param11+param33+param22")]
+        [DataRow("param1=1&param3=3&param2=2", "&", "param11&param33&param22")]
+        public void FetchParameterNameAndValueTest2(string input, string separator, string result)
+        {
+            var builder = SignatureBuilder.Create(input).FetchParameterNameAndValue(separator);
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+
+        [TestMethod()]
+        [DataRow("param3=3+&param1=1&param2=2", "param1=1&param2=2&param3=3+")]
+        public void OrderByAsyncTest(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).OrderBy();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "dGVzdDE9MSZ0ZXN0Mj0y")]
+        public void Base64Test(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).Base64();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "appKey", "appKeytest1=1&test2=2")]
+        public void PrefixTest(string input, string prefix, string result)
+        {
+            var builder = SignatureBuilder.Create(input).Prefix(prefix);
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "appKey", "test1=1&test2=2appKey")]
+        public void PostfixTest(string input, string postfix, string result)
+        {
+            var builder = SignatureBuilder.Create(input).Postfix(postfix);
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "ee2511b7137adee95204899d1855eb3f")]
+        public void MD5Test(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).MD5();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "e4e44e7539fff69b6fb984ef206aedaf9b3c650f")]
+        public void SHA1Test(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).SHA1();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "8ebc3e5c2302d3500e7a7e1dcb5e9ff65d1b0c304133cca9cb656e8f67275a9e")]
+        public void SHA256Test(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).SHA256();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("test1=1&test2=2", "TEST1=1&TEST2=2")]
+        public void ToUpperTest(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).ToUpper();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("Test1=1&Test2=2", "test1=1&test2=2")]
+        public void ToLowerTest(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).ToLower();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3+&param2=2", "param1%3D1%26param3%3D3%2B%26param2%3D2")]
+        public void UrlEncodeUpperTest(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).UrlEncodeUpper();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+
+        [TestMethod()]
+        [DataRow("param1=1&param3=3+&param2=2", "param1%3d1%26param3%3d3%2b%26param2%3d2")]
+        public void UrlEncodeLowerTest(string input, string result)
+        {
+            var builder = SignatureBuilder.Create(input).UrlEncodeLower();
+            Assert.IsTrue(result == builder.GetResult());
+        }
+    }
+}
---
+++ src/JiuLing.CommonLibs/Security/Signature/SignatureBuilder.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace JiuLing.CommonLibs.Security.Signature
+{
+    /// <summary>
+    /// 签名构建器
+    /// </summary>
+    public class SignatureBuilder
+    {
+        /// <summary>
+        /// 最终的签名串
+        /// </summary>
+        private string _signValue;
+
+        /// <summary>
+        /// 原始签名串
+        /// </summary>
+        /// <param name="signValue"></param>
+        private SignatureBuilder(string signValue)
+        {
+            _signValue = signValue;
+        }
+
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        /// <param name="parameters">要签名的数据</param>
+        /// <returns></returns>
+        public static SignatureBuilder Create(IDictionary<string, string> parameters)
+        {
+            var keyValue = parameters.Select(x => $"{x.Key}={x.Value}").ToList();
+            return new SignatureBuilder(string.Join("&", keyValue));
+        }
+
+        /// <summary>
+        /// 初始化数据
+        /// </summary>
+        /// <param name="parameters">要签名的数据</param>
+        /// <returns></returns>
+        public static SignatureBuilder Create(string parameters)
+        {
+            return new SignatureBuilder(parameters);
+        }
+
+        /// <summary>
+        /// 字典序
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder OrderBy()
+        {
+            var parameters = _signValue.Split('&').ToList().OrderBy(x => x).ToList();
+            _signValue = string.Join("&", parameters);
+            return this;
+        }
+
+        /// <summary>
+        /// 提取参数值
+        /// </summary>
+        /// <param name="separator">提取后的参数连接符</param>
+        /// <returns></returns>
+        public SignatureBuilder FetchParameterValue(string separator = "&")
+        {
+            var parameterKeyValueList = _signValue.Split('&').ToList();
+            var parameterValueList = parameterKeyValueList.Select(x => x.Split('=')[1]).ToList();
+            _signValue = string.Join(separator, parameterValueList);
+            return this;
+        }
+
+        /// <summary>
+        /// 提取参数名和参数值
+        /// </summary>
+        /// <param name="separator">提取后的参数连接符</param>
+        /// <returns></returns>
+        public SignatureBuilder FetchParameterNameAndValue(string separator = "")
+        {
+            var parameterKeyValueList = _signValue.Split('&').ToList();
+            parameterKeyValueList = parameterKeyValueList.Select(x => x.Replace("=", "")).ToList();
+            _signValue = string.Join(separator, parameterKeyValueList);
+            return this;
+        }
+
+        /// <summary>
+        /// Base64
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder Base64()
+        {
+            _signValue = Base64Utils.StringToBase64(_signValue);
+            return this;
+        }
+
+        /// <summary>
+        /// 加前缀
+        /// </summary>
+        /// <param name="value">前缀值</param>
+        /// <returns></returns>
+        public SignatureBuilder Prefix(string value)
+        {
+            _signValue = value + _signValue;
+            return this;
+        }
+
+        /// <summary>
+        /// 加后缀
+        /// </summary>
+        /// <param name="value">后缀值</param>
+        /// <returns></returns>
+        public SignatureBuilder Postfix(string value)
+        {
+            _signValue = _signValue + value;
+            return this;
+        }
+
+        /// <summary>
+        /// MD5
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder MD5()
+        {
+            _signValue = MD5Utils.GetStringValueToLower(_signValue);
+            return this;
+        }
+
+        /// <summary>
+        /// SHA1
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder SHA1()
+        {
+            _signValue = SHA1Utils.GetStringValueToLower(_signValue);
+            return this;
+        }
+
+        /// <summary>
+        /// SHA256
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder SHA256()
+        {
+            _signValue = SHA256Utils.GetStringValueToLower(_signValue);
+            return this;
+        }
+
+        /// <summary>
+        /// 转大写
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder ToUpper()
+        {
+            _signValue = _signValue.ToUpper();
+            return this;
+        }
+
+        /// <summary>
+        /// 转小写
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder ToLower()
+        {
+            _signValue = _signValue.ToLower();
+            return this;
+        }
+
+        /// <summary>
+        /// 小写 UrlEncode (1%3d1%262%3d2)
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder UrlEncodeLower()
+        {
+            _signValue = HttpUtility.UrlEncode(_signValue);
+            return this;
+        }
+
+        /// <summary>
+        /// 大写 UrlEncode (1%3D1%262%3D2)
+        /// </summary>
+        /// <returns></returns>
+        public SignatureBuilder UrlEncodeUpper()
+        {
+            var sb = new StringBuilder();
+            foreach (char s in _signValue)
+            {
+                var encode = HttpUtility.UrlEncode(s.ToString());
+                if (encode.Length > 1)
+                {
+                    sb.Append(encode.ToUpper());
+                }
+                else
+                {
+                    sb.Append(encode);
+                }
+            }
+            _signValue = sb.ToString();
+            return this;
+        }
+
+        /// <summary>
+        /// 获取签名结果
+        /// </summary>
+        /// <returns></returns>
+        public string GetResult()
+        {
+            return _signValue;
+        }
+    }
+}
