69679ebef4ece59e17dec379e399e2b21637eea2
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2024-09-06 11:05:46 +0800

    新增 Version 对象输出指定格式的扩展方法

---
+++ src/JiuLing.CommonLibs.UnitTests/ExtensionMethods/VersionExtensionTests.cs
@@ -0,0 +1,43 @@
+﻿using JiuLing.CommonLibs.Enums;
+using JiuLing.CommonLibs.ExtensionMethods;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace JiuLing.CommonLibs.UnitTests.ExtensionMethods
+{
+    [TestClass()]
+    public class VersionExtensionTests
+    {
+        [TestMethod()]
+        [DataRow("1.2", VersionFormatEnum.Major, "1")]
+        [DataRow("1.2", VersionFormatEnum.MajorMinor, "1.2")]
+        [DataRow("1.2", VersionFormatEnum.MajorMinorBuild, "1.2.0")]
+        [DataRow("1.2", VersionFormatEnum.MajorMinorBuildRevision, "1.2.0.0")]
+        [DataRow("1.2.3", VersionFormatEnum.Major, "1")]
+        [DataRow("1.2.3", VersionFormatEnum.MajorMinor, "1.2")]
+        [DataRow("1.2.3", VersionFormatEnum.MajorMinorBuild, "1.2.3")]
+        [DataRow("1.2.3", VersionFormatEnum.MajorMinorBuildRevision, "1.2.3.0")]
+        [DataRow("1.2.3.4", VersionFormatEnum.Major, "1")]
+        [DataRow("1.2.3.4", VersionFormatEnum.MajorMinor, "1.2")]
+        [DataRow("1.2.3.4", VersionFormatEnum.MajorMinorBuild, "1.2.3")]
+        [DataRow("1.2.3.4", VersionFormatEnum.MajorMinorBuildRevision, "1.2.3.4")]
+        public void ToFormatVersionTest1(string input, VersionFormatEnum format, string result)
+        {
+            var version = new Version(input);
+            var value = version.ToFormatString(format);
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod()]
+        public void ToFormatVersionTest1_ArgumentOutOfRangeException()
+        {
+            var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            {
+                (new Version("1.2")).ToFormatString((VersionFormatEnum)0);
+            });
+
+            Assert.AreEqual("format", exception.ParamName);
+            Assert.IsTrue(exception.Message.IndexOf("无效的版本格式") >= 0);
+        }
+    }
+}
---
+++ src/JiuLing.CommonLibs/ExtensionMethods/VersionExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using JiuLing.CommonLibs.Enums;
+
+namespace JiuLing.CommonLibs.ExtensionMethods
+{
+    /// <summary>
+    /// 版本号的扩展方法
+    /// </summary>
+    public static class VersionExtension
+    {
+        /// <summary>
+        /// 格式化版本号
+        /// </summary>
+        /// <param name="version">版本号</param>
+        /// <param name="format">格式化</param>
+        /// <returns>格式化后的版本号字符串</returns>
+        public static string ToFormatString(this Version version, VersionFormatEnum format)
+        {
+            switch (format)
+            {
+                case VersionFormatEnum.Major:
+                    return version.Major.ToString();
+                case VersionFormatEnum.MajorMinor:
+                    return $"{version.Major}.{version.Minor}";
+                case VersionFormatEnum.MajorMinorBuild:
+                    return $"{version.Major}.{version.Minor}.{GetVersionBuild(version)}";
+                case VersionFormatEnum.MajorMinorBuildRevision:
+                    return $"{version.Major}.{version.Minor}.{GetVersionBuild(version)}.{GetVersionRevision(version)}";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(format), "无效的版本格式");
+            }
+        }
+
+        private static int GetVersionBuild(Version version)
+        {
+            var build = version.Build;
+            if (build == -1)
+            {
+                build = 0;
+            }
+            return build;
+        }
+        private static int GetVersionRevision(Version version)
+        {
+            var revision = version.Revision;
+            if (revision == -1)
+            {
+                revision = 0;
+            }
+            return revision;
+        }
+    }
+}
